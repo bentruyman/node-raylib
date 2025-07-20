9a04617d4a9354b0900c211a4836e6f2a7181599
Ben Truyman <ben@truyman.com>
2025-07-20 08:05:05 -0500

    Use Prettier and ESLint (with unjs config) to format and lint code

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import unjs from "eslint-config-unjs";
+
+export default unjs({
+  ignores: ["src/generated"],
+  rules: {
+    "@typescript-eslint/no-require-imports": "off",
+    "unicorn/no-process-exit": "off",
+    "unicorn/prefer-top-level-await": "off",
+  },
+});
